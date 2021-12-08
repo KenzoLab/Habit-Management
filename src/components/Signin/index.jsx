4a9d25fa904a24a2b0548eeb4495b8072f07283d
theo gandara <theogandara@gmail.com>
2021-12-08 17:48:45 -0300

    feat: Added styles and yup resolvers

--- src/components/Signin/index.jsx
+++ src/components/Signin/index.jsx
@@ -1,35 +1,84 @@
 import { TextField, Button } from "@mui/material";
 import { Link } from "react-router-dom";
+import habit from "../../assets/habit-mobile.png";
+import { Container } from "./styles";
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
 const Signin = () => {
+  const formSchema = yup.object().shape({
+    email: yup.string().required("Enter your email*").email("Email inválido"),
+    password: yup.string().required("Register a password*").min(6, "Senha fraca"),
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(formSchema),
+  });
+
+  const onSubmitFunction = (data) => {
+    console.log(data);
+  };
+
   return (
-    <>
-      <img src="../assets/habit.png" alt="logo"></img>
+    <Container>
+      <img src={habit} alt="logo" />
+
       <h1>Welcome</h1>
 
-      <TextField
-        className="Inputs"
-        id="outlined-basic"
-        label="Email"
-        variant="outlined"
-      />
+      <form onSubmit={handleSubmit(onSubmitFunction)}>
+        <TextField
+          {...register("email")}
+          className="Inputs"
+          id="outlined-basic"
+          label="Email"
+          variant="outlined"
+          sx={{
+            margin: 1,
+          }}
+        />
+        <p>{errors.email?.message}</p>
 
-      <TextField
-        className="Inputs"
-        id="outlined-basic"
-        label="Password"
-        variant="outlined"
-      />
+        <TextField
+          {...register("password")}
+          type="password"
+          className="Inputs"
+          id="outlined-basic"
+          label="Password"
+          variant="outlined"
+          sx={{
+            margin: 1,
+          }}
+        />
 
-      <Button className="Button" variant="contained">
-        Sign In
-      </Button>
+        <p>{errors.password?.message}</p>
+        <Button
+          type="Submit"
+          className="Button"
+          variant="contained"
+          sx={{
+            margin: 2,
+            bgcolor: "#38B6FF",
+            width: 230,
+            "&:hover": {
+              backgroundColor: "#72c1f1",
+            },
+          }}
+        >
+          Sign In
+        </Button>
+      </form>
 
       <hr className="line" />
 
       <span>
-        If you odn't have an account, sign up <Link to="/">here</Link>!
+        If you don't have an account, sign up<Link to="/"> here</Link>!
       </span>
-    </>
+    </Container>
   );
 };
 
--- src/components/Signin/styles.js
+++ src/components/Signin/styles.js
@@ -1,3 +1,48 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
-const 
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  
+  img {
+    width: 180px;
+    margin: 30px 0px 20px 0px;
+  }
+
+  h1{
+    font-size: 40px;
+  }
+  
+  hr {
+    color: #081140;
+    width: 80%;
+    background-color: #081140;
+    margin-bottom: 10px;
+  }
+  
+  span{
+    margin-top: 15px;
+    font-size: 14px;
+  }
+  
+  a{
+    color: #38B6FF;
+    cursor: pointer;
+    text-decoration: none;
+  }
+
+  p{
+      margin: 0;
+      padding: 0;
+      font-size: 12px;
+      color: #dc1f1f;
+  }
+
+  form{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  
+  `;
