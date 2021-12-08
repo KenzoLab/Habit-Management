c99b745e7a056c08b08cac6fddecab9a886b139c
Rafael Oliveira Carvalho <rafaeloc.dev@gmail.com>
2021-12-08 00:15:44 -0300

    feat: create global style

---
+++ src/styles/global.js
@@ -0,0 +1,23 @@
+﻿import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  body, main, div, ul, li, a, p, figure, img, input, button {
+    margin: 0;  
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: 'Roboto', sans-serif;
+    background-color: #ececec;
+  }
+
+  ul {
+    list-style-type: none;
+  }
+
+  a {
+    text-decoration: none;
+    color: #333;
+  }
+`;
