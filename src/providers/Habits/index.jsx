c94914e14dd76b70b2dbad0dff938faed0517e90
Rafael Oliveira Carvalho <rafaeloc.dev@gmail.com>
2021-12-08 17:41:36 -0300

    feat: creating provider to create a habit

---
+++ src/providers/Habits/index.jsx
@@ -0,0 +1,32 @@
+﻿import { createContext, useContext, useState } from 'react';
+import api from '../../services/api';
+
+const HabitContext = createContext({});
+
+const HabitProvider = ({ children }) => {
+  const [errorMessage, setErrorMessage] = useState('');
+  const token = localStorage.getItem('@Habit:token');
+  const AuthorizationObj = {
+    headers: { Authorization: `Bearer ${token}` },
+  };
+
+  const createHabit = (formData) => {
+    api
+      .post('/habits/', formData, AuthorizationObj)
+      .then(
+        (response) =>
+          console.log(response.data) /* toast register habit success */,
+      )
+      .catch((error) => setErrorMessage(error.message));
+  };
+
+  return (
+    <HabitContext.Provider value={{ createHabit, errorMessage }}>
+      {children}
+    </HabitContext.Provider>
+  );
+};
+
+const useHabit = () => useContext(HabitContext);
+
+export { HabitProvider, useHabit };
