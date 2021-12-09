a66f750c2588ddb9c6062c95524a34aae9f2a37e
Rafael Oliveira Carvalho <rafaeloc.dev@gmail.com>
2021-12-09 00:55:08 -0300

    feat: creating provider to create a group

--- src/providers/Groups/index.jsx
+++ src/providers/Groups/index.jsx
@@ -5,6 +5,7 @@ const GroupsContext = createContext({});
 
 const GroupsProvider = ({ children }) => {
   const [errorMessage, setErrorMessage] = useState("");
+  const [lastCreatedGroup, setLastCreatedGroup] = useState({});
   const [allGroupsList, setAllGroupsList] = useState([]);
 
   const token = localStorage.getItem("@Habit:token");
@@ -21,9 +22,29 @@ const GroupsProvider = ({ children }) => {
     }
   };
 
+  const createGroupFunction = async (formData) => {
+    console.log(formData);
+    try {
+      const response = await api.post("/groups/", formData, AuthorizationObj);
+      setLastCreatedGroup(response.data);
+    } catch (error) {
+      setErrorMessage(error);
+    }
+    // api
+    //   .post("/groups/", formData, AuthorizationObj)
+    //   .then((response) => console.log(response.data))
+    //   .catch((error) => console.log(error));
+  };
+
   return (
     <GroupsContext.Provider
-      value={{ errorMessage, listGroupsFunction, allGroupsList }}>
+      value={{
+        errorMessage,
+        listGroupsFunction,
+        allGroupsList,
+        createGroupFunction,
+        lastCreatedGroup,
+      }}>
       {children}
     </GroupsContext.Provider>
   );
