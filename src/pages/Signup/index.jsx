6764cce5c417512b5e106e7baf3b6c11301d7b68
breno santos <breno.angelim@hotmail.com>
2021-12-09 23:17:48 -0300

    codigo comentado pra revisao

--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -1,107 +1,164 @@
-﻿import { useForm } from 'react-hook-form';
-import * as yup from 'yup';
-import { yupResolver } from '@hookform/resolvers/yup';
-import { useHistory, Redirect, Link } from 'react-router-dom';
-import { toast } from 'react-hot-toast';
-import { Container, Section, Header, Form } from './styles'
-import logo from '../../assets/logoSemSlogan-removebg-preview.png';
-
-import { api } from '../../services/api.js';
-
-/*
-
-
-*/
+﻿import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { useHistory, Redirect, Link } from "react-router-dom";
+import { toast } from "react-hot-toast";
+import {
+  Container,
+  Section,
+  Header,
+  Form,
+  DivChild,
+  Input,
+  Label,
+  Paragr,
+} from "./styles";
+import logo from "../../assets/logoSemSlogan-removebg-preview.png";
+import { api } from "../../services/api.js";
 
 const SignUp = () => {
-    const history = useHistory();
+  const history = useHistory();
 
-        const schema = yup.object().shape({
-            username: yup.string().required('Campo obrigatório'),
-            email: yup
-                .string()
-                .email('Email inválido')
-                .required('Campo obrigatório'),
-            password: yup
-                .string()
-                .required('Campo obrigatório')
-                .min(6, 'A senha deve conter pelo menos 6 caracteres'),
-            confirm_password: yup
-                .string()
-                .required('Campo obrigatório')
-                .oneOf([yup.ref('password'), null], 'As senhas devem ser iguais'),
-        })
+  const schema = yup.object().shape({
+    username: yup.string().required("Campo obrigatório"),
+    email: yup.string().email("Email inválido").required("Campo obrigatório"),
+    password: yup
+      .string()
+      .required("Campo obrigatório")
+      .min(6, "A senha deve conter pelo menos 6 caracteres"),
+    confirm_password: yup
+      .string()
+      .required("Campo obrigatório")
+      .oneOf([yup.ref("password"), null], "As senhas devem ser iguais"),
+  });
 
-        const { register, 
-                handleSubmit, 
-                formState:{ errors }, 
-            } = useForm({
-                resolver: yupResolver(schema)
-            });
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
+  const handleRegisterSubmit = (data) => {
+    delete data.confirm_password;
+    console.log(data);
+  };
 
-        const handleRegisterSubmit = data =>{ 
-            
-            delete data.confirm_password;
-
-            api
-                .post('/users', data)
-                .then((response) => {
-                    toast.success('Yeah! Cadastro realizado com sucesso!')
-                    console.log(response)
-                    return history.push('/');
-                })
-                .catch((err) =>
-                    toast.error('Usuário já cadastrado, dirija-se à página de login!'))
-        }
-
-        return (
-            <Container>
-            <Header>
-                <img src={logo} alt='' />
-            </Header>
-            <Section>
-                <h1>Signup</h1>
-                <Form onSubmit={handleSubmit(handleRegisterSubmit)}>
-                <h2>Name:</h2>
-                <input
-                className='input'
-                placeholder="Name"
+  return (
+    <Container>
+      <Header>
+        <img src={logo} alt="" />
+      </Header>
+      <Section>
+        <h1>Signup</h1>
+        <Form onSubmit={handleSubmit(handleRegisterSubmit)}>
+          {/*<DivChild>
+            <div className="abs">
+              <Input
+                id="username"
+                type="text"
+                pattern="[A-Za-záàâãéèêíïóôõöúçñÁÀÂÃÉÈÍÏÓÔÕÖÚÇÑ ]+$"
+                autoComplete="username"
+                placeholder=" "
+                required=""
                 {...register("username")}
-                />
-                <p className='error-message'>{errors.username?.message}</p>                
-                <h2>Email:</h2>
-                <input
-                className='input'
-                placeholder="Email"
-                {...register("email")}
-                />
-                <p className='error-message'>{errors.email?.message}</p>   
-                <h2>Password:</h2>
-                <input
-                className='input'
-                type='password'
-                placeholder="Password"
-                {...register("password")}
-                />
-                <p className='error-message'>{errors.password?.message}</p>
-                <h2>Confirm password:</h2>
-                <input
-                className='input'
-                type='password'
-                placeholder="Confirm password"
-                {...register("confirm_password")}
-                />   
-                <p className='error-message'>{errors.confirm_password?.message}</p>
-                </Form>
-                <button type='submit'>Sign Up</button>
+              ></Input>
+              <Label>Name:</Label>
+            </div>
+            <Paragr>{errors.username?.message}</Paragr>
+          </DivChild>*/}
 
-                <p className='message'>
-                If you have already registered, login <Link to='/'>here</Link>!
-                </p>
-            </Section>
-            </Container>
-        )
-}
+          <h2>Name:</h2>
+          <input
+            className="input"
+            placeholder="Name"
+            {...register("username")}
+          />
+          <p className="error-message">{errors.username?.message}</p>
+
+          {/*<DivChild>
+            <div className="abs">
+              <Input
+                id="Email"
+                type="text"
+                autoComplete="email"
+                placeholder=" "
+                required=""
+                {...register("email")}
+              ></Input>
+              <Label>Email:</Label>
+            </div>
+            <Paragr>{errors.email?.message}</Paragr>
+          </DivChild>*/}
+
+          <h2>Email:</h2>
+          <input className="input" placeholder="Email" {...register("email")} />
+          <p className="error-message">{errors.email?.message}</p>
+
+          <h2>Password:</h2>
+          <input
+            className="input"
+            type="password"
+            placeholder="Password"
+            {...register("password")}
+          />
+          <p className="error-message">{errors.password?.message}</p>
+
+          {/*<DivChild>
+            <div className="abs">
+              <Input
+                id="password"
+                type="text"
+                autoComplete="password"
+                placeholder=" "
+                required=""
+                type="password"
+                {...register("password")}
+              ></Input>
+              <Label>Password:</Label>
+            </div>
+            <Paragr>{errors.password?.message}</Paragr>
+          </DivChild>*/}
+
+          <h2>Confirm password:</h2>
+          <input
+            className="input"
+            type="password"
+            placeholder="Confirm password"
+            {...register("confirm_password")}
+          />
+          <p className="error-message">{errors.confirm_password?.message}</p>
+
+          {/*<DivChild>
+            <div className="abs">
+              <Input
+                id="confirm_password"
+                type="text"
+                autoComplete="confirm_password"
+                placeholder=" "
+                required=""
+                type="password"
+                {...register("confirm_password")}
+              ></Input>
+              <Label>Confirm password:</Label>
+            </div>
+            <Paragr>{errors.confirm_password?.message}</Paragr>
+          </DivChild>*/}
+
+          <button type="submit">Sign Up</button>
+        </Form>
+
+        <p className="message">
+          If you have already registered, login{" "}
+          <Link to="/" id="link">
+            here
+          </Link>
+          !
+        </p>
+      </Section>
+    </Container>
+  );
+};
 
 export default SignUp;
--- src/pages/Signup/styles.js
+++ src/pages/Signup/styles.js
@@ -1,75 +1,257 @@
-﻿import styled from 'styled-components';
-import '../../fonts/FiraSans/FiraSans-Medium.ttf';
-import '../../fonts/FiraSans/FiraSans-Regular.ttf';
-import '../../fonts/FiraSans/FiraSans-SemiBold.ttf';
+﻿import styled from "styled-components";
+import "../../fonts/FiraSans/FiraSans-Medium.ttf";
+import "../../fonts/FiraSans/FiraSans-Regular.ttf";
+import "../../fonts/FiraSans/FiraSans-SemiBold.ttf";
 
 export const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  @media (min-width: 768px) {
+    flex-direction: row-reverse;
+    height: 100vh;
+    justify-content: space-evenly;
+  }
 `;
 
 export const Header = styled.header`
-    img{
-        width: 150px;
-        margin: 20px 0 0 0;
-    }
-`
+  img {
+    width: 150px;
+    margin: 20px 0 0 0;
+  }
+`;
 
 export const Section = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-
-    h1 {
-        font-size: 24px;
+  h1 {
+    font-size: 24px;
+    @media (min-width: 768px) {
+      font-size: 52px;
     }
-    h2 {
-        font-size: 12px;
-    }
+  }
+  h2 {
+    font-size: 12px;
+  }
 
-    button {
-        width: 239px;
-        height: 30px;
-        background-color: #38B6FF;
-        color: #FFFFFF;
+  button {
+    width: 239px;
+    height: 30px;
+    background-color: #38b6ff;
+    color: #ffffff;
 
-        font-size: 12px;
-        font-weight: 600;
+    font-size: 12px;
+    font-weight: 600;
 
-        border: none;
-        border-radius: 5px;
+    border: none;
+    border-radius: 5px;
 
-        margin: 15px 0px 20px 0px;
-    }
+    margin: 15px 0px 20px 0px;
+  }
 
-    .message {
-        font-size: 10px;
-        border-top: 1px solid #9398A2; 
-        padding: 15px;
-    }
-`
+  .message {
+    font-size: 12px;
+    border-top: 1px solid #9398a2;
+    padding: 15px;
+  }
+
+  #link {
+    color: #38b6ff;
+  }
+`;
 
 export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+
+  .input {
+    width: 239px;
+    height: 30px;
+    margin-bottom: 15px;
+
+    border: none;
+    background-color: rgba(222, 222, 222, 0.4);
+    border-radius: 5px;
+
+    padding-left: 10px;
+
+    font-size: 12px;
+  }
+`;
+
+//Seção - Input do Bruno: alterado pelo Breno
+export const Label = styled.label`
+  color: #081140;
+  opacity: 50%;
+  pointer-events: none;
+  position: absolute;
+  top: calc(50% - 5px);
+  font-size: 14px;
+  transition: all 0.2s linear;
+  -webkit-transition: all 0.2s linear;
+  -moz-transition: all 0.2s linear;
+  background-color: transparent;
+  box-sizing: border-box;
+  align-self: flex-start;
+`;
+
+export const Input = styled.input`
+  font-size: 14px;
+  width: 239px;
+  height: 30px;
+  padding-left: 10px;
+  margin-top: 30px;
+  text-align: flex-start;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: solid 1px #ffffff;
+  opacity: 50%;
+  background-color: #dedede;
+  color: #081140;
+  outline: none;
+  transition: all 0.2s linear;
+  -webkit-transition: all 0.2s linear;
+  -moz-transition: all 0.2s linear;
+  -webkit-appearance: none;
+
+  &:focus {
+    border-bottom: 2px solid #ffffff;
+    opacity: 100%;
+  }
+
+  &:hover {
+    border-bottom: 2px solid #ffffff;
+    opacity: 100%;
+  }
+
+  &::placeholder {
+    color: transparent;
+  }
+
+  &:required:invalid + label {
+    color: red;
+    opacity: 100%;
+  }
+
+  &:focus:required:invalid {
+    border-bottom: solid 2px red;
+    opacity: 100%;
+  }
+
+  &:required:invalid + label:before {
+    content: "*";
+    opacity: 100%;
+  }
+
+  &:focus + label {
+    font-size: 13px;
+    margin-top: 2px;
+    top: 0;
+    color: #081140;
+  }
+
+  &:not(:placeholder-shown) + label {
+    opacity: 100%;
+    font-size: 13px;
+    top: 0;
+    color: #081140;
+  }
+`;
+
+export const Paragr = styled.p`
+  font-size: 11px;
+  color: red;
+  height: 20px;
+  margin-top: 5px;
+  align-self: flex-start;
+`;
+
+export const DivChild = styled(Container)`
+  width: 100%;
+  height: 80px;
+  margin: 5px 0px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  align-content: flex-start;
+  position: relative;
+
+  .abs {
     display: flex;
     flex-direction: column;
-    justify-content: center;
-    align-items: flex-start;
-    
-    .input {
-        width: 239px;
-        height: 30px;
-        margin-bottom: 15px;
+  }
+`;
 
-        border: none;
-        background-color: rgba(222, 222, 222, 0.4);
-        border-radius: 5px;
+/**
+ * export const DivFather = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 90%;
+  height: 700px;
+  background-color: transparent;
+  margin-top: 100px;
 
-        padding-left: 10px;
+  @media (min-width: 768px) {
+    width: 500px;
+  }
+`;
+ * 
 
-        font-size: 12px;
+export const TittleH2 = styled.h2`
+  font-size: 20px;
+  font-weight: 600;
+  text-transform: uppercase;
+  color: rgb(255, 255, 255);
+  padding-top: 5px;
+  margin: 10px 0px;
+`;
+ * 
+ export const Form = styled.form`
+   background-color: transparent;
+   padding: 20px 35px;
+   width: 100%;
+   height: 90%;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   justify-content: center;
+   box-sizing: border-box;
+ `;
+
+export const Button = styled.button`
+  width: 150px;
+  padding: 12px 30px;
+  margin-top: 50px;
+  font-weight: 500;
+  border-radius: 20px;
+  border: solid 1px #0c5e5a;
+  color: #127c77;
+  background-color: transparent;
+  align-self: center;
+
+  @media (min-width: 1024px) {
+    align-self: flex-end;
+
+    &:hover {
+      cursor: pointer;
+      background-color: #0f8191;
+      border: solid 1px #0f8191;
+      color: white;
+      font-weight: 600;
+      opacity: 90%;
     }
-`
+  }
+`;
+
+
+ */
