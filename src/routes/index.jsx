d7e9974852fc49598b565b23fd5a6965798c705e
Rafael Oliveira Carvalho <rafaeloc.dev@gmail.com>
2021-12-12 18:35:47 -0300

    style: update files

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,9 +4,9 @@ import Route from "./routes";
 import Signin from "../pages/Signin";
 import Signup from "./../pages/Signup";
 import Dashboard from "../pages/Dashboard";
-// import Groups from "../pages/Groups";
-// import Activities from "../pages/Activities";
-// import Goals from "../pages/Goals";
+import Groups from "../pages/Groups";
+import Activities from "../pages/Activities";
+import Goals from "../pages/Goals";
 
 const Routes = () => {
   return (
@@ -14,9 +14,9 @@ const Routes = () => {
       <Route exact path="/" component={Signin} />
       <Route path="/signup" component={Signup} />
       <Route path="/dashboard" component={Dashboard} isPrivate />
-      {/* <Route path="/groups" component={Groups} isPrivate />
+      <Route path="/groups" component={Groups} isPrivate />
       <Route path="/activities" component={Activities} isPrivate />
-      <Route path="/goals" component={Goals} isPrivate /> */}
+      <Route path="/goals" component={Goals} isPrivate />
     </Switch>
   );
 };
