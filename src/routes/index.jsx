f990ac5315558d88232fe229bf8935a7f7436739
Rafael Oliveira Carvalho <rafaeloc.dev@gmail.com>
2021-12-12 17:50:37 -0300

    feat: creating and applyed protected routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,19 +4,19 @@ import Route from "./routes";
 import Signin from "../pages/Signin";
 import Signup from "./../pages/Signup";
 import Dashboard from "../pages/Dashboard";
-import Groups from "../pages/Groups";
-import Activities from "../pages/Activities";
-import Goals from "../pages/Goals";
+// import Groups from "../pages/Groups";
+// import Activities from "../pages/Activities";
+// import Goals from "../pages/Goals";
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path="/" component={Signin} />
-      <Route path="/dashboard" component={Dashboard} isPrivate />
       <Route path="/signup" component={Signup} />
-      <Route path="/groups" component={Groups} />
-      <Route path="/activities" component={Activities} />
-      <Route path="/goals" component={Goals} />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      {/* <Route path="/groups" component={Groups} isPrivate />
+      <Route path="/activities" component={Activities} isPrivate />
+      <Route path="/goals" component={Goals} isPrivate /> */}
     </Switch>
   );
 };
