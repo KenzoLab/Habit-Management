0b8fa6dfeec9693c504e1a4ac79fd1c97e953f0b
Rafael Oliveira Carvalho <rafaeloc.dev@gmail.com>
2021-12-13 15:13:38 -0300

    feat: added CardFrame component

---
+++ src/components/CardFrame/index.jsx
@@ -0,0 +1,12 @@
+﻿import { Container } from "./styles";
+
+const CardFrame = ({ children }) => {
+  return (
+    <Container>
+      {children}
+      <div className="back"></div>
+    </Container>
+  );
+};
+
+export default CardFrame;
---
+++ src/components/CardFrame/styles.js
@@ -0,0 +1,20 @@
+﻿import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 234px;
+  width: 600px;
+  border-radius: 34px;
+  background-color: #fff;
+  position: relative;
+
+  .back {
+    width: 100%;
+    height: 100%;
+    background-color: var(--yellow);
+    border-radius: 34px;
+    position: absolute;
+    z-index: -1;
+    top: 0;
+    left: 15px;
+  }
+`;
