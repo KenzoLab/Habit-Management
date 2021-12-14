3e859d8418fff161a2f25a5db1f55926bb45221a
breno santos <breno.angelim@hotmail.com>
2021-12-14 08:09:02 -0300

    feat! - Dashboard - structure jsx groups and style groups cards

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,1 +1,115 @@
-﻿
+﻿import { App, Countainer, Aside, Header, Cards } from "./styles";
+
+function Dashboard() {
+  const cards = ["zeze", "gugu", "dada", "ZEUS!"];
+
+  return (
+    <App>
+      <Aside>
+        <header>
+          <img alt="Logo" />
+        </header>
+        <section>
+          <div className="pages">Dashboard</div>
+          <div className="pages">Groupes</div>
+          <div className="pages">activities</div>
+          <div className="pages">goals</div>
+          <div className="pages">Logout</div>
+        </section>
+        <footer>
+          <div id="usuario-card">
+            <img alt="usuario" />
+            <div>
+              <h3>Nome</h3>
+              <p>Email</p>
+            </div>
+          </div>
+        </footer>
+      </Aside>
+      <Countainer>
+        <Header>
+          <section className="header-top">
+            <h2>Groups</h2>
+            <div className="header-search">
+              <input
+                placeholder="search..."
+                //onChange={(evt) setSearch(evt.target.value)}
+              />
+              <button
+              //onClick={() => addGroup()}
+              >
+                + add grupo
+              </button>
+            </div>
+          </section>
+
+          <section className="header-bottom">
+            <div id="blues">
+              <button>Today</button>
+              <button>week</button>
+              <button>Month</button>
+            </div>
+          </section>
+        </Header>
+        <Cards>
+          {cards.map((nome) => (
+            <div className="card-border" key={nome}>
+              <div className="card">
+                <div className="card-conteudo">
+                  <div className="card-header">
+                    <h2>{nome}</h2>
+                    <p>metas: 10</p>
+                  </div>
+
+                  <div className="card-descrption">
+                    <p>
+                      Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                      Quidem, ea obcaecati. Vitae nostrum omnis, explicabo
+                      temporibus eaque officia repudiandae tempora. Vel
+                      molestiae harum reiciendis illo enim pariatur dolorum
+                      dolorem nemo!
+                    </p>
+                    <details>
+                      <summary>Participantes</summary>
+                      <ul>
+                        <li>Breno</li>
+                        <li>Bruno</li>
+                        <li>Brebo</li>
+                        <li>Evandro</li>
+                        <li>Loro</li>
+                        <li>José</li>
+                        <li>Faustão</li>
+                      </ul>
+                    </details>
+                  </div>
+                </div>
+
+                <hr className="card-descrption-border" />
+
+                <div className="card-butons">
+                  <button
+                  //onClick={() => entratNoGrupo()}
+                  >
+                    subcribe
+                  </button>
+                  <button
+                  //onClick={() => ModalAtividader()}
+                  >
+                    atividades
+                  </button>
+                  <button
+                  //onClick={() => ModalMetas()}
+                  >
+                    goals
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </Cards>
+      </Countainer>
+    </App>
+  );
+}
+
+export default Dashboard;
--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -1,1 +1,174 @@
-﻿
+﻿import styled from "styled-components";
+
+export const App = styled.section`
+  display: flex;
+  flex-direction: column;
+  @media (min-width: 600px) {
+    flex-direction: row;
+  }
+`;
+
+export const Countainer = styled.section`
+  width: 100vw;
+  height: 86vh;
+  background-color: #dedede;
+  display: flex;
+  flex-direction: column;
+  @media (min-width: 600px) {
+    height: 100vh;
+    min-width: 68vw;
+  }
+`;
+
+export const Aside = styled.aside`
+  width: 100vw;
+  height: 14vh;
+  display: flex;
+  flex-direction: row;
+  background-color: lightgray;
+  justify-content: space-between;
+  @media (min-width: 600px) {
+    width: 32vw;
+    max-width: 260px;
+    height: 100vh;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+`;
+
+export const Header = styled.header`
+  height: 22vh;
+  background-color: #dedede;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
+  @media (max-width: 600px) {
+    height: 12vh;
+  }
+  .header-search {
+    margin-right: 10%;
+    input {
+      padding: 7px 16px;
+      font-size: 16px;
+      border-radius: 9px;
+      border: none;
+      margin-right: 12px;
+    }
+    button {
+      padding: 7px 16px;
+      background-color: #72c1f1;
+      border-radius: 9px;
+      border: none;
+    }
+  }
+  .header-top {
+    width: 100%;
+    height: 50%;
+    margin-bottom: 4px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    @media (max-width: 600px) {
+      display: none;
+    }
+    h2 {
+      font-size: 24px;
+      margin: 0px 8px 0px 19px;
+    }
+  }
+  .header-bottom {
+    width: 95%;
+    margin-left: 35px;
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    @media (max-width: 600px) {
+      align-items: center;
+      justify-content: center;
+      margin-left: 0px;
+    }
+    button {
+      padding: 7px 15px;
+      margin: 6px 10px;
+      background: transparent;
+      color: #081140;
+      font-weight: bold;
+      font-size: 20px;
+      border: none;
+      border-radius: 7px;
+      font-weight: 600;
+      @media (max-width: 600px) {
+        font-size: calc(12px + 1.5vmin);
+        margin: 6px 7px;
+      }
+    }
+    button:focus {
+      background: #72c1f1;
+      opacity: 70%;
+    }
+  }
+  #blues {
+    background-color: #dedede;
+  }
+`;
+
+export const Cards = styled.section`
+  margin: auto;
+  width: 95%;
+  height: 82vh;
+  overflow-y: auto;
+  border-radius: 10px;
+  font-size: 14px;
+  h2 {
+    font-size: 24px;
+    margin: 5px 10px;
+  }
+  .card-border {
+    padding-right: 10px;
+    background-color: #fabd59;
+    border-radius: 10px;
+    margin: 0px 20px;
+  }
+  .card-descrption-border {
+  }
+  .card {
+    height: 160px;
+    margin: 30px 0px;
+    padding: 10px 0px;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: row;
+    background-color: #f2f0f0;
+    border-radius: 10px;
+    @media (max-width: 600px) {
+      margin: 20px 0px;
+    }
+  }
+  .card-header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+  .card-conteudo {
+    //border-right: 2px solid #9398a2;
+    width: 90%;
+    padding: 0px 8px;
+  }
+  .card-descrption {
+    height: 80px;
+    overflow-y: auto;
+  }
+  .card-butons {
+    margin: 0px 17px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    button {
+      border: none;
+      color: blue;
+      @media (max-width: 600px) {
+      }
+    }
+  }
+`;
