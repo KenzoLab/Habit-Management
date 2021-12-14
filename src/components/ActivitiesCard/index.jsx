f308c042843773f95ecebee4453e2130e78f0a4a
Rafael Oliveira Carvalho <rafaeloc.dev@gmail.com>
2021-12-14 07:19:37 -0300

    feat: creating ActivitiesCard component

--- src/components/ActivitiesCard/index.jsx
+++ src/components/ActivitiesCard/index.jsx
@@ -1,1 +1,19 @@
-﻿
+﻿import CardFrame from "../CardFrame";
+import { ContentContainer, Status } from "./styles";
+
+const ActivitiesCard = ({ title, group, date, status }) => {
+  return (
+    <CardFrame cardType="activities">
+      <ContentContainer>
+        <h4 className="content__title">{title}</h4>
+        <p className="content__group">{group}</p>
+        <p className="content__date">
+          Date: <span>{date}</span>
+        </p>
+        <Status>{status}</Status>
+      </ContentContainer>
+    </CardFrame>
+  );
+};
+
+export default ActivitiesCard;
