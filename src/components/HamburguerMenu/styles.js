716cbd931d28ca273d2418bdc6467cfb26f69566
Nicky <nicollyporfirio12@gmail.com>
2021-12-14 12:21:17 -0300

    Feat: added desktop version

--- src/components/HamburguerMenu/styles.js
+++ src/components/HamburguerMenu/styles.js
@@ -1,44 +1,5 @@
 ﻿import styled from "styled-components";
 
-export const Footer = styled.div`
-  section {
-    display: ${props => (props.toggleNav ? "block" : "none")};
-    width: 80vw;
-    align-items: center;
-  }
-  .username {
-    font-size: 15px;
-    color: #081140;
-  }
-  .useremail {
-    font-size: 12px;
-    font-style: italic;
-    color: #9398a2;
-  }
-`;
-
-export const Header = styled.div`
-  width: 100vw;
-  height: 13.4vh;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding-top: 20px;
-
-  img {
-    width: 130px;
-    margin-right: 25px;
-  }
-
-  button {
-    margin-left: 25px;
-    border: 1px solid transparent;
-    background-color: transparent;
-    width: 25px;
-    height: 25px;
-  }
-`;
-
 export const Nav = styled.div`
   display: flex;
   box-sizing: border-box;
@@ -101,5 +62,98 @@ export const Nav = styled.div`
   }
 
   @media (min-width: 601px) {
+    width: 250px;
+    height: 100vh;
+    .menu-list {
+      width: 250px;
+
+      section {
+        width: 250px;
+        display: flex;
+        align-items: center;
+        ul {
+          flex-direction: column;
+          width: 200px;
+          justify-content: center;
+
+          li {
+            display: flex;
+            align-items: center;
+            height: 50px;
+            border: none;
+            margin: 0px 0px 10px 15px;
+          }
+
+          a {
+            text-decoration: none;
+            color: #081140;
+            margin-top: 6px;
+            font-size: 17px;
+            font-family: "Fira Sans";
+          }
+        }
+      }
+
+      div {
+        width: 250px;
+      }
+    }
+  }
+`;
+
+export const Header = styled.div`
+  width: 100vw;
+  height: 13.4vh;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-top: 20px;
+
+  img {
+    width: 130px;
+    margin-right: 25px;
+  }
+
+  button {
+    margin-left: 25px;
+    border: 1px solid transparent;
+    background-color: transparent;
+    width: 25px;
+    height: 25px;
+  }
+
+  @media (min-width: 601px) {
+    width: 250px;
+    button {
+      display: none;
+    }
+
+    img {
+      width: 130px;
+      margin: auto;
+    }
+  }
+`;
+
+export const Footer = styled.div`
+  section {
+    display: ${props => (props.toggleNav ? "block" : "none")};
+    width: 80vw;
+    align-items: center;
+  }
+  .username {
+    font-size: 15px;
+    color: #081140;
+  }
+  .useremail {
+    font-size: 12px;
+    font-style: italic;
+    color: #9398a2;
+  }
+
+  @media (min-width: 601px) {
+    section {
+      margin-bottom: 10px;
+    }
   }
 `;
