3b179cfd7d7f495bab3e5a1586a9c457675be5df
Marília Calado <mgcd.calado@gmail.com>
2021-12-15 16:55:37 -0300

    feature: speed dial button

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -4,6 +4,7 @@ import HabitCard from "../../components/HabitCard";
 import { useHabit } from "../../providers/Habits";
 import { useAuth } from "../../providers/AuthProvider";
 import { useEffect } from "react";
+import BasicSpeedDial from '../../components/SpeedDialHabits';
 
 function Dashboard() {
   const { listHabitsFunction, listHabits } = useHabit();
@@ -73,61 +74,9 @@ function Dashboard() {
               category={habit.category}
               difficult={habit.difficult}
             />
-            //   <div className="card-border" key={nome}>
-            //     <div className="card">
-            //       <div className="card-conteudo">
-            //         <div className="card-header">
-            //           <h2>{nome}</h2>
-            //           <p>metas: 10</p>
-            //         </div>
-
-            //         <div className="card-description">
-            //           <p>
-            //             Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-            //             Quidem, ea obcaecati. Vitae nostrum omnis, explicabo
-            //             temporibus eaque officia repudiandae tempora. Vel
-            //             molestiae harum reiciendis illo enim pariatur dolorum
-            //             dolorem nemo!
-            //           </p>
-            //           <details>
-            //             <summary>Participantes</summary>
-            //             <ul>
-            //               <li>Breno</li>
-            //               <li>Bruno</li>
-            //               <li>Breno</li>
-            //               <li>Evandro</li>
-            //               <li>Loro</li>
-            //               <li>José</li>
-            //               <li>Faustão</li>
-            //             </ul>
-            //           </details>
-            //         </div>
-            //       </div>
-
-            //       <hr className="card-description-border" />
-
-            //       <div className="card-buttons">
-            //         <button
-            //         //onClick={() => entratNoGrupo()}
-            //         >
-            //           subscribe
-            //         </button>
-            //         <button
-            //         //onClick={() => ModalAtividader()}
-            //         >
-            //           atividades
-            //         </button>
-            //         <button
-            //         //onClick={() => ModalMetas()}
-            //         >
-            //           goals
-            //         </button>
-            //       </div>
-            //     </div>
-            //   </div>
-            //
           ))}
         </Cards>
+        <BasicSpeedDial />
       </Container>
     </App>
   );
