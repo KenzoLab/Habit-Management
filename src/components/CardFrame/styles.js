6b68968c237516a19df6a02ac1edd565757b6a51
Rafael Oliveira Carvalho <rafaeloc.dev@gmail.com>
2021-12-15 12:07:59 -0300

    style: update file

--- src/components/CardFrame/styles.js
+++ src/components/CardFrame/styles.js
@@ -8,24 +8,26 @@ export const Container = styled.div`
     if (props.cardType === "activities" || props.cardType === "goals") {
       return "350px";
     } else {
-      return "600px";
+      return "650px";
     }
   }};
   border-radius: 15px;
-  background-color: #fff;
+  background-color: var(--yellow);
   position: relative;
   padding: 25px;
+  padding-left: 20px;
   box-sizing: border-box;
+  transform: translateX(15px);
 
   ::after {
     content: "";
     height: 100%;
     width: 100%;
     border-radius: 15px;
-    background-color: var(--yellow);
+    background-color: var(--white);
     position: absolute;
     top: 0;
-    left: 10px;
+    left: -10px;
     z-index: -1;
   }
   @media (min-width: 600px) {
