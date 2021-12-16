abc78c64e56813da5231456f579c9b62cff0c07b
Rafael Oliveira Carvalho <rafaeloc.dev@gmail.com>
2021-12-16 02:11:35 -0300

    refactor: change psudo element after to border right

--- src/components/CardFrame/styles.js
+++ src/components/CardFrame/styles.js
@@ -14,17 +14,16 @@ export const Container = styled.div`
   border-radius: 30px;
   position: relative;
   padding: 25px;
+  border-right: 12px solid var(--yellow);
   background-color: var(--white);
   padding-left: 20px;
   box-sizing: border-box;
-  transform: translateX(15px);
+  /* transform: translateX(15px); */
   margin: 10px;
 
-  box-shadow: -4px -2px 11px 0px rgba(147,152,162,1);
+  box-shadow: -4px -2px 11px 0px rgba(147, 152, 162, 1);
 
-
-
-  ::after {
+  /* ::after {
     content: "";
     height: 100%;
     width: 99%;
@@ -34,7 +33,7 @@ export const Container = styled.div`
     top: 0;
     left: -2px;
     z-index: -1;
-  }
+  } */
   @media (min-width: 600px) {
     border-radius: 20px;
     border-right: 12px solid var(--yellow);
