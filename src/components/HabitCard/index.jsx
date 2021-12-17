9ecfe999792fae692dd4fe12684ecb2165291753
Marília Calado <mgcd.calado@gmail.com>
2021-12-17 03:43:00 -0300

    feature: habits card

--- src/components/HabitCard/index.jsx
+++ src/components/HabitCard/index.jsx
@@ -1,5 +1,5 @@
-﻿import CardFrame from "../CardFrame";
-import { ContentContainer, Difficulty, LineVertical } from "./styles";
+﻿
+import { ButtonContainer, ContentContainer, Difficulty, LineVertical, CardFrame } from "./styles";
 import { useHabit } from "../../providers/Habits";
 
 // achieved: false
@@ -19,17 +19,16 @@ const HabitCard = ({ title, category, frequency, difficulty, habitId }) => {
   return (
     <CardFrame cardType="habit">
       <ContentContainer cardType="habit">
-        {/*<div className="content__text">
           <h4 className="content__title">{title}</h4>
           <p className="content__frequency">{frequency}</p>
           <span className="content__category">{category}</span>
           <Difficulty>{difficulty}</Difficulty>
-        </div>
-        <LineVertical />
-        <div className="delete__button" onClick={onDeleteHabit}>
-          X
-  </div>*/}
       </ContentContainer>
+        <ButtonContainer>
+          <button className="delete__button" onClick={onDeleteHabit}>
+            x
+          </button>
+        </ButtonContainer>
     </CardFrame>
   );
 };
